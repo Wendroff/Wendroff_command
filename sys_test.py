0a6447a8434d17cdf07d8fb9be18cd89fdd77cac
Wendroff <624101034@qq.com>
2017-12-08 14:30:34 +0800

    test output

--- sys_test.py
+++ sys_test.py
@@ -7,8 +7,14 @@ Created on Thu Dec  7 14:36:43 2017
 
 import sys
 #print(len(sys.argv))
+hello = open('hello.dat','w')
 for thing in sys.argv :
-	print(thing)
+    print(thing)
+    hello.write(thing+'\n')
 
 name = input("Please input your name:\n")
+
+hello.write("Hello "+name+'\n')
 print("Hello,", name)
+
+hello.close()
